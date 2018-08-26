d30c31a75d40d91fc401f8e57b511396886bfc3f
jwma <mjw@majiawei.com>
2018-08-26 19:41:32 +0800

    管理后台应用 - 添加vue.config.js，设置开发环境的接口反向代理

---
+++ admin-app/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  }
+}
