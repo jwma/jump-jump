afe384184f0b339df085724d154779ed9ba03004
MJ <mjw@majiawei.com>
2020-03-01 18:43:48 +0800

    refactor(v2): 删除不需要做的 todo

--- internal/app/handlers/redirect.go
+++ internal/app/handlers/redirect.go
@@ -20,7 +20,5 @@ func Redirect(c *gin.Context) {
 	h := models.NewRequestHistory(l, c.Request.RemoteAddr, c.Request.UserAgent())
 	_ = h.Save() // 因为需要继续处理重定向，所以保存请求记录失败不做处理
 
-	// TODO 更新短链接点击次数（使用一个独立的计数器来增加点击次数）
-
 	c.Redirect(http.StatusMovedPermanently, l.Url)
 }
