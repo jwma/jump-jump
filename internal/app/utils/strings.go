4b10b5cfcac6cc87ffb963efd166403d9aff47f3
MJ <mjw@majiawei.com>
2020-03-04 17:58:13 +0800

    refactor(v2): 拆分 utils

--- internal/app/utils/strings.go
+++ internal/app/utils/strings.go
@@ -2,11 +2,13 @@ package utils
 
 import (
 	"math/rand"
+	"os"
 	"time"
 )
 
 var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyz")
 var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+var secretKey = os.Getenv("SECRET_KEY")
 
 func RandStringRunes(n int) string {
 	b := make([]rune, n)
