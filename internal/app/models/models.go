9738f660f16d42d7f5fc616377de3c6a8568a230
MJ <mjw@majiawei.com>
2020-03-13 02:06:41 +0800

    feat(v2): 增加用户短链接记录, 全局短链接记录
    记录使用 redis sorted set 存储，用来维护用户和短链接之间的关系。
    用户每创建、删除一个短链接，都会删除该短链接对应用户的短链接记录。
    
    全局短链接记录作用类似，但主要目的是为了后续 RoleAdmin 角色能够方便
    获取全部短链接列表。

--- internal/app/models/models.go
+++ internal/app/models/models.go
@@ -123,12 +123,23 @@ func (s *ShortLink) Save() error {
 	if s.Url == "" {
 		return fmt.Errorf("请填写url")
 	}
+	if s.CreatedBy == "" {
+		return fmt.Errorf("未设置创建者，请通过接口创建短链接")
+	}
 
 	s.CreateTime = time.Now()
 	s.UpdateTime = time.Now()
 	c := db.GetRedisClient()
 	j, _ := json.Marshal(s)
+	// 保存短链接
 	c.Set(s.key(), j, 0)
+	// 保存用户的短链接记录，保存到创建者及全局
+	record := redis.Z{
+		Score:  float64(time.Now().Unix()),
+		Member: s.Id,
+	}
+	c.ZAdd(fmt.Sprintf("links:%s", s.CreatedBy), record)
+	c.ZAdd("links", record)
 
 	return nil
 }
@@ -168,6 +179,9 @@ func (s *ShortLink) Delete() {
 
 	// 删除短链接本身
 	c.Del(s.key())
+	// 删除用户的短链接记录及全局短链接记录
+	c.ZRem(fmt.Sprintf("links:%s", s.CreatedBy), s.Id)
+	c.ZRem("links", s.Id)
 
 	// 删除访问历史
 	keys, _ := c.Keys(fmt.Sprintf("history:%s:*", s.Id)).Result()
