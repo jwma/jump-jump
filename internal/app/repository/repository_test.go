ae9f2c3e6e5f2eab83f621c7cf1903ee2fb294b8
MJ <mjw@majiawei.com>
2020-04-13 15:57:07 +0800

    test(request history): 添加访问历史记录 repo 测试代码

--- internal/app/repository/repository_test.go
+++ internal/app/repository/repository_test.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jwma/jump-jump/internal/app/models"
 	"github.com/jwma/jump-jump/internal/app/utils"
 	"testing"
+	"time"
 )
 
 func getTestRDB() *redis.Client {
@@ -12,6 +13,7 @@ func getTestRDB() *redis.Client {
 }
 
 func init() {
+	// 清空测试使用的数据库，以便后续测试正常运作
 	getTestRDB().FlushDB()
 }
 
@@ -35,8 +37,8 @@ func TestShortLinkRepository_Save(t *testing.T) {
 func TestShortLinkRepository_Get(t *testing.T) {
 	id := "mj"
 	repo := GetShortLinkRepo(getTestRDB())
-
 	_, err := repo.Get(id)
+
 	if err != nil {
 		t.Error(err)
 	}
@@ -45,8 +47,8 @@ func TestShortLinkRepository_Get(t *testing.T) {
 func TestShortLinkRepository_Update(t *testing.T) {
 	id := "mj"
 	repo := GetShortLinkRepo(getTestRDB())
-
 	l, err := repo.Get(id)
+
 	if err != nil {
 		t.Error(err)
 	}
@@ -82,11 +84,86 @@ func TestShortLinkRepository_List(t *testing.T) {
 func TestShortLinkRepository_Delete(t *testing.T) {
 	id := "mj"
 	repo := GetShortLinkRepo(getTestRDB())
-
 	l, err := repo.Get(id)
+
 	if err != nil {
 		t.Error(err)
 	}
 
 	repo.Delete(l)
+}
+
+func TestRequestHistoryRepository_Save(t *testing.T) {
+	l := &models.ShortLink{
+		Id:          "testrh",
+		Url:         "http://anmuji.com",
+		Description: "",
+		IsEnable:    true,
+		CreatedBy:   "mj",
+	}
+	slRepo := GetShortLinkRepo(getTestRDB())
+	err := slRepo.Save(l)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	rh := models.NewRequestHistory(l, "127.0.0.1", "fake user agent")
+	rhRepo := GetRequestHistoryRepo(getTestRDB())
+	rhRepo.Save(rh)
+}
+
+func TestRequestHistoryRepository_FindLatest(t *testing.T) {
+	id := "testrh"
+	rhRepo := GetRequestHistoryRepo(getTestRDB())
+	rs, err := rhRepo.FindLatest(id, 10)
+	expected := 1
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rs.Total != expected {
+		t.Errorf("expected %b but gog %b\n", expected, rs.Total)
+	}
+}
+
+func TestRequestHistoryRepository_FindByDate(t *testing.T) {
+	id := "testrh"
+	rhRepo := GetRequestHistoryRepo(getTestRDB())
+	rs, err := rhRepo.FindByDate(id, time.Now())
+	expected := 1
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rs.Total != expected {
+		t.Errorf("expected %b but gog %b\n", expected, rs.Total)
+	}
+}
+
+func TestRequestHistoryRepository_FindByDateRange(t *testing.T) {
+	id := "testrh"
+	rhRepo := GetRequestHistoryRepo(getTestRDB())
+	start := time.Now()
+	end := start.Add(time.Hour * 24)
+
+	// 开始日期大于结束日期
+	_, err := rhRepo.FindByDate(id, end, start)
+	if err == nil {
+		t.Errorf("expected error but got nil")
+	}
+
+	// 正常情况
+	rs, err := rhRepo.FindByDate(id, start, end)
+	expected := 1
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if rs.Total != expected {
+		t.Errorf("expected %b but gog %b\n", expected, rs.Total)
+	}
 }
